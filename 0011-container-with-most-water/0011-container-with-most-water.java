83bcc359d56fdbe03c88a86465b22806110a754e
Ujjawal Baliyan <ubaliyan808@gmail.com>
2026-07-11 15:01:31 +0530

    Time: 5 ms (82.59%), Space: 77.2 MB (68.46%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,23 @@
+// This code can be done using left and right pointer!!!
+class Solution {
+    public  int maxArea(int height[]) {
+        int maxWater=0;
+        // for(int i=0;i<height.length;i++){
+              int lp=0;
+              int rp=height.length-1;
+              while(lp<rp){
+                int ht=Math.min(height[lp],height[rp]);
+                int width=rp-lp;
+                int currWater=ht * width;
+                maxWater=Math.max(currWater,maxWater);
+              if(height[lp]<height[rp]){
+                lp++;
+              }
+              else{
+                rp--;
+            }
+        }
+    // }    
+     return maxWater;  
+}
+}
